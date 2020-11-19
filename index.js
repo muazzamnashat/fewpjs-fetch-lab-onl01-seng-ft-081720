c78958c1c9ad5497bb30028cb0a7a86c512415af
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-19 09:30:54 -0500

    commit tree d2fcedd3f47594f6132c9f83b3de0b22dc878cdd

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -12,5 +14,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+   fetchBooks()
 })
+
